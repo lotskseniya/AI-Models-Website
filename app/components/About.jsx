caaa2dd3848a48bd45d50c2e0d66a27b799b6ddc
lotskseniya <99911595+lotskseniya@users.noreply.github.com>
2025-05-17 19:54:02 +0300

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -70,11 +70,7 @@ const About = () => {
               objectFit: "contain",
               objectPosition: "center",
               zIndex: "1",
-<<<<<<< HEAD
               marginTop: "-5dvh",
-=======
-              marginTop: "-8dvh",
->>>>>>> a15dfb163178e1bb47e1274b294a3afbeb368e80
               marginLeft: "5dvw",
             }}
             ref={imageAboutRef}
@@ -88,11 +84,7 @@ const About = () => {
             justifyContent: "center",
             color: "rgba(255, 255, 255, 1)",
             alignItems: "start",
-<<<<<<< HEAD
             padding: "3rem 5rem 0 5rem",
-=======
-            padding: "6.5dvw 5rem 0 5rem",
->>>>>>> a15dfb163178e1bb47e1274b294a3afbeb368e80
             width: "48.25vw",
           }}
           className="aboutTextContainer"
